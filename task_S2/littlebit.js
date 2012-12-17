d6a1c1986e19b2b84ea7a07cb8d62add73b69540
EugenAz <myreg@inbox.ru>
2012-12-17 11:48:29 +0200

    Update task_S2/littlebit.js

--- task_S2/littlebit.js
+++ task_S2/littlebit.js
@@ -1,8 +1,5 @@
 $(function(){
-
 	var boxWidth = 216;
-	var dblBoxWidth = 442;
-	
 	var curCol = 1;
 	var colOneLeft = 0;
 	var colTwoLeft = boxWidth+10;
@@ -10,31 +7,78 @@ $(function(){
 	var colOneTop = 0;
 	var colTwoTop = 0;
 	var colThreeTop = 0;
-	
-	$(".infobox").each(function(){
-		var curHeight = $(this).outerHeight();
-		
-		if(curCol == 1){
-			$(this).css("left", colOneLeft + "px");
-			$(this).css("top", colOneTop + "px");
-			colOneTop += curHeight + 10;
-			curCol = 2;
-		}
-		else if(curCol == 2){
-			$(this).css("left", colTwoLeft + "px");
-			$(this).css("top", colTwoTop + "px");
-			colTwoTop += curHeight + 10;
-			curCol = 3;
-		}
-		else{
-			$(this).css("left", colThreeLeft + "px");
-			$(this).css("top", colThreeTop + "px");
-			colThreeTop += curHeight + 10;	
-			curCol = 1;
-		}
+	var boxes = $(".infobox");
+	/*var sumHeight = 0;
+	var blocksQty = boxes.length;
+	boxes.each(function(){
+		sumHeight += $(this).outerHeight();
+	});
+	var avgHeight = Math.round(sumHeight/blocksQty);*/
+	// 1
+	var boxesOrder = new Array;
+	var i = 0;
+	$(".big").each(function(){		
+		boxesOrder[i*3] = [$(".infobox").index(this),'b'];
+		boxesOrder[i*3+1] = ['b'];
+		i++;
 	});
 	
-	
+	boxes.each(function(){
+		if($(this).hasClass("big"))	return;
+		for(var i=0;;i++){
+			if(boxesOrder[i] == null){
+				boxesOrder[i] = [$(".infobox").index(this),'s'];
+				break;
+			}
+			else{
+				continue;
+			}
+		}
+	});
+	// 2
+	var heightLimit = 150;
+	$.each( boxesOrder, function(i, obj){
+		if(obj[0] != 'b'){
+			
+			thisObj = $(".infobox").eq(obj[0]);
+			var curHeight = thisObj.outerHeight();
+			if(curCol == 1){
+				thisObj.css("left", colOneLeft + "px");
+				thisObj.css("top", colOneTop + "px");
+				colOneTop += curHeight + 10;
+				if(boxesOrder[i+1][0] != 'b'){
+					curCol = 2;
+				}
+				else{
+					curCol = 3;
+					colTwoTop += curHeight + 10;
+				}
+			}
+			else if(curCol == 2){
+				thisObj.css("left", colTwoLeft + "px");
+				thisObj.css("top", colTwoTop + "px");
+				colTwoTop += curHeight + 10;
+				if((colOneTop - colTwoTop) > heightLimit){
+					curCol = 2;
+				}
+				else{
+					curCol = 3;
+				}
+			}
+			else{
+				thisObj.css("left", colThreeLeft + "px");
+				thisObj.css("top", colThreeTop + "px");
+				colThreeTop += curHeight + 10;	
+				if((colTwoTop - colThreeTop) > heightLimit){
+					curCol = 3;
+				}
+				else{
+					curCol = 1;
+				}
+			}
+		}
+		
+	});
 	// main_part->height()
 	if(colOneTop>colTwoTop){
 		if(colOneTop>colThreeTop){
@@ -52,5 +96,4 @@ $(function(){
 			$(".main_part").css("height", colThreeTop);
 		}
 	}
-	
 });
