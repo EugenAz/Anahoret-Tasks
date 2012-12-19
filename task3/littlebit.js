1caa5062408a965757c8a3ca7afc19849d3a1bb0
EugenAz <myreg@inbox.ru>
2012-12-19 15:39:56 +0200

    Update task3/littlebit.js

--- task3/littlebit.js
+++ task3/littlebit.js
@@ -1,32 +1,31 @@
 $(function(){
-	personOpened = 0;
-	function makeChanges(msgIndex){
-		$(".person").css("display", "none");
-		$(".pic_holder").css("z-index", "0");
-		
-		$(".msg:eq(" + msgIndex + ") .pic_holder").css("z-index", "100");
-		$(".msg:eq(" + msgIndex + ") .person").css("display", "block");
-		
-		$("body").append("<div id=\"closePerson\"></div>");
-		var closeP = $("#closePerson");
-		closeP.width($(document).width());
-		closeP.height($(document).height());
-		closeP.mouseover(function(){
-			$(".msg:eq(" + msgIndex + ") .person").hide();
-			$(this).width(0).height(0);
-		});
-		var name = $(".msg:eq(" + msgIndex + ") .name");
-		var nameText = name.html();
-		$(".msg:eq(" + msgIndex + ") .person .person_name").html(nameText);
-	}
-	
-	$(".pic_holder").click(function(evt){
-		var msgIndex = $(".pic_holder").index(this);
-		makeChanges(msgIndex);
-	});
-	
-	$(".msg .name").click(function(evt){
-		var msgIndex = $(".msg .name").index(this);
-		makeChanges(msgIndex);
-	});
+  function makeChanges(msgIndex){
+    $(".person").css("display", "none");
+    $(".pic_holder").css("z-index", "0");
+    
+    $(".msg:eq(" + msgIndex + ") .pic_holder").css("z-index", "100");
+    $(".msg:eq(" + msgIndex + ") .person").css("display", "block");
+    
+    $("body").append("<div id=\"closePerson\"></div>");
+    var closeP = $("#closePerson");
+    closeP.width($(document).width());
+    closeP.height($(document).height());
+    closeP.mouseover(function(){
+      $(".msg:eq(" + msgIndex + ") .person").hide();
+      $(this).width(0).height(0);
+    });
+    var name = $(".msg:eq(" + msgIndex + ") .name");
+    var nameText = name.html();
+    $(".msg:eq(" + msgIndex + ") .person .person_name").html(nameText);
+  }
+  
+  $(".pic_holder").click(function(){
+    var msgIndex = $(".pic_holder").index(this);
+    makeChanges(msgIndex);
+  });
+  
+  $(".msg .name").click(function(){
+    var msgIndex = $(".msg .name").index(this);
+    makeChanges(msgIndex);
+  });
 });
